68459647ab74b08978f68307b4b84e9aeb97865b
Almir Braggio <almirbraggio@gmail.com>
2017-07-19 22:14:32 -0300

    Rename files

---
+++ 02/src/btree.c
@@ -0,0 +1,85 @@
+// Author: Almir Braggio
+// jun. 2017
+
+#include "bin_tree.h"
+
+// init
+bintree_t init_bintree (void) {
+	bintree_t tree = NULL;
+	return tree;
+}
+
+// free
+bintree_t free_bintree (bintree_t node) {
+	if (isempty_bintree(node))
+		return NULL;
+	free_bintree(node->left);
+	free_bintree(node->right);
+	
+	free(node);
+	return NULL;
+}
+
+// empty
+bool_t isempty_bintree (bintree_t node) {
+	return (bool_t)(node == NULL);
+}
+
+// insert
+bintree_t insert_bintree (bintree_t node, data_t data) {
+	if (isempty_bintree(node)) {
+		node = (struct bintree_node_t *)malloc(sizeof(struct bintree_node_t));
+		node->data = data;
+		node->left = node->right = NULL; 
+	}
+	else {
+		if (data < node->data)
+			node->left = insert_bintree(node->left, data);
+		else if (data > node->data)
+			node->right = insert_bintree(node->right, data);
+	}
+	return node;
+}
+
+// search
+data_t min_bintree (bintree_t node) {
+	bintree_t r = node;
+	while (r->left != NULL)
+		r =  r->left;
+	return r->data;
+}
+
+data_t max_bintree (bintree_t node) {
+	bintree_t r = node;
+	while (r->right != NULL)
+		r =  r->right;
+	return r->data;
+}
+
+// prints
+void preorder_bintree(bintree_t node) {
+	if (!isempty_bintree(node)) {
+		printf("%d ", node->data);
+		preorder_bintree(node->left);
+		preorder_bintree(node->right);
+	}
+	return;
+}
+
+void inorder_bintree(bintree_t node) {
+	if (!isempty_bintree(node)) {
+		inorder_bintree(node->left);
+		printf("%d ", node->data);
+		inorder_bintree(node->right);
+	}
+	return;
+}
+
+void postorder_bintree(bintree_t node) {
+	if (!isempty_bintree(node)) {
+		postorder_bintree(node->left);
+		postorder_bintree(node->right);
+		printf("%d ", node->data);
+	}
+	return;
+}
---
+++ 02/src/btree.h
@@ -0,0 +1,52 @@
+// Author: Almir Braggio
+// jun. 2017
+
+#ifndef BIN_TREE_H_INCLUDED
+#define BIN_TREE_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifndef NULL
+#define NULL    (0)
+#endif
+
+#ifndef bool_t
+#define bool_t 	unsigned char
+#define true	1
+#define false	0
+#endif
+
+#define UNUSED(x) (void)(x)
+
+typedef unsigned int data_t;
+typedef struct bintree_node_t *bintree_t;
+
+struct bintree_node_t {
+	data_t data;
+	bintree_t left, right;
+};
+
+// init
+bintree_t init_bintree (void);
+
+// free
+bintree_t free_bintree (bintree_t node);
+
+// empty
+bool_t isempty_bintree (bintree_t node);
+
+// insert
+bintree_t insert_bintree (bintree_t node, data_t data);
+
+// search
+data_t min_bintree (bintree_t node);
+data_t max_bintree (bintree_t node);
+
+// prints
+void preorder_bintree(bintree_t node);
+void inorder_bintree(bintree_t node);
+void postorder_bintree(bintree_t node);
+
+#endif
