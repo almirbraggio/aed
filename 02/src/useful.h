2ef9737d3ae518aa1e383ba21d8342051ceac919
Almir Braggio <almirbraggio@gmail.com>
2017-07-20 21:43:57 -0300

    Rename files

--- 02/src/useful.h
+++ 02/src/useful.h
@@ -4,10 +4,18 @@
 #ifndef UTIL_H_INCLUDED
 #define UTIL_H_INCLUDED
 
+#ifndef bool_t
+#define bool_t 		unsigned char
+#define true		1
+#define false		0
+#endif
+
 #ifndef NULL
 #define NULL    	(0)
 #endif
 
+#ifndef UNUSED
 #define UNUSED(x) 	(void)(x)
+#endif
 
 #endif
