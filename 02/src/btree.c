d1c4a6a1af7a4da9084d06242807717cfaf7c9a1
Almir Braggio <almirbraggio@gmail.com>
2017-07-20 22:46:44 -0300

    Remove unused code

--- 02/src/btree.c
+++ 02/src/btree.c
@@ -9,6 +9,19 @@ btree_t *init_btree (void) {
 	return root;
 }
 
+/*
+// free
+bintree_t free_bintree (bintree_t node) {
+	if (isempty_bintree(node))
+		return NULL;
+	free_bintree(node->left);
+	free_bintree(node->right);
+	
+	free(node);
+	return NULL;
+}
+*/
+
 // insert
 bool_t insert_btree (btree_t **node, uint key) {
 	btree_t *new_node = NULL;
@@ -220,80 +233,18 @@ uint max_btree (btree_t *node) {
 	return max;
 }
 
-
 /*
-// free
-bintree_t free_bintree (bintree_t node) {
-	if (isempty_bintree(node))
-		return NULL;
-	free_bintree(node->left);
-	free_bintree(node->right);
-	
-	free(node);
-	return NULL;
-}
-
-// empty
-bool_t isempty_bintree (bintree_t node) {
-	return (bool_t)(node == NULL);
-}
-
-// insert
-bintree_t insert_bintree (bintree_t node, data_t data) {
-	if (isempty_bintree(node)) {
-		node = (struct bintree_node_t *)malloc(sizeof(struct bintree_node_t));
-		node->data = data;
-		node->left = node->right = NULL; 
+// finds the maximum level
+int max_level_btree (btree_t *node) {
+	if (node) {
+		int l = 0, mr = 0, r = 0, max_depth;
+		if (node->p[0] != NULL) l = maxLevel(node->p[0]);
+		if (node->p[1] != NULL) mr = maxLevel(node->p[1]);	
+		if (node->n == 2) {
+			if (node->p[2] != NULL) r = maxLevel(node->p[2]);
+		}
+		max_depth = max(l, mr, r) + 1;
+		return max_depth;
 	}
-	else {
-		if (data < node->data)
-			node->left = insert_bintree(node->left, data);
-		else if (data > node->data)
-			node->right = insert_bintree(node->right, data);
-	}
-	return node;
-}
-
-// search
-data_t min_bintree (bintree_t node) {
-	bintree_t r = node;
-	while (r->left != NULL)
-		r =  r->left;
-	return r->data;
-}
-
-data_t max_bintree (bintree_t node) {
-	bintree_t r = node;
-	while (r->right != NULL)
-		r =  r->right;
-	return r->data;
-}
-
-// prints
-void preorder_bintree(bintree_t node) {
-	if (!isempty_bintree(node)) {
-		printf("%d ", node->data);
-		preorder_bintree(node->left);
-		preorder_bintree(node->right);
-	}
-	return;
-}
-
-void inorder_bintree(bintree_t node) {
-	if (!isempty_bintree(node)) {
-		inorder_bintree(node->left);
-		printf("%d ", node->data);
-		inorder_bintree(node->right);
-	}
-	return;
-}
-
-void postorder_bintree(bintree_t node) {
-	if (!isempty_bintree(node)) {
-		postorder_bintree(node->left);
-		postorder_bintree(node->right);
-		printf("%d ", node->data);
-	}
-	return;
-}
-*/
+	return 0;
+}*/
--- 02/src/btree.h
+++ 02/src/btree.h
@@ -61,25 +61,4 @@ int total_btree (btree_t *node);
 uint min_btree (btree_t *node);
 uint max_btree (btree_t *node);
 
-
-
-/*
-// free
-bintree_t free_bintree (bintree_t node);
-
-// empty
-bool_t isempty_bintree (bintree_t node);
-
-// insert
-bintree_t insert_bintree (bintree_t node, data_t data);
-
-// search
-data_t min_bintree (bintree_t node);
-data_t max_bintree (bintree_t node);
-
-// prints
-void preorder_bintree(bintree_t node);
-void inorder_bintree(bintree_t node);
-void postorder_bintree(bintree_t node);
-*/
 #endif
